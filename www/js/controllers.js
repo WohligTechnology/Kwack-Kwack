088129518b9b3f938d757108d1897afff5213039
Pranay Joshi <pranayj1449517@gmail.com>
2017-11-09 12:27:24 +0530

    invitefriends

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -81,6 +81,22 @@ angular.module('starter.controllers', [])
   $scope.contact = function(){
     $scope.toggle = false
   }
+   $scope.followersb = true;
+  $scope.followers=function(){
+    $scope.followersb = true;
+    $scope.kwacksb = false;
+    $scope.pollsb = false;
+  }
+  $scope.kwacks=function(){
+    $scope.followersb = false;
+    $scope.kwacksb = true;
+    $scope.pollsb = false;
+  }
+  $scope.polls=function(){
+    $scope.followersb = false;
+    $scope.pollsb = true;
+    $scope.kwacksb = false;
+  }
 })
 
 .controller('SrartPollingCtrl', function($scope) {
