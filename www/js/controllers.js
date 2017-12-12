bfe24c3ec294055272e137d1c962848ae621bc41
sohan1 <shn619@gmail.com>
2017-12-12 11:22:27 +0530

    tariler ctrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,21 +52,7 @@ var connector=angular.module('starter.controllers', [ 'starter.controllers'])
   setupSlider();
 })
 
-.controller('TrailerCtrl', function($scope, $ionicModal) {
-  $ionicModal.fromTemplateUrl('templates/modal/filter1.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function (modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function () {
-    $scope.modal.show();
-  }
 
-  $scope.closeModal = function () {
-    $scope.modal.hide();
-  };
-})
 
 .controller('DiscoverCtrl', function($scope, $ionicModal) {
   $ionicModal.fromTemplateUrl('templates/modal/filter1.html', {
