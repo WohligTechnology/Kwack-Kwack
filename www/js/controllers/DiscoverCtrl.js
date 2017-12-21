0b4d3c3b885cb7d86c958b74794723f0024925c9
Pranay Joshi <pranayj1449517@gmail.com>
2017-12-21 13:04:31 +0530

    interestdone ngclassremaining

--- www/js/controllers/DiscoverCtrl.js
+++ www/js/controllers/DiscoverCtrl.js
@@ -1,4 +1,12 @@
 connector.controller('DiscoverCtrl', function($scope,Chats, $ionicModal) {
+$scope.jstorage={}
+$scope.jstorage = $.jStorage.get('user');
+$scope.addInterest={}
+$scope.addInterest.userId= $scope.jstorage._id
+$scope.addInterest.interest=[]
+console.log("heyyjstorage",$scope.addInterest)
+
+$scope.interest=[]
     $ionicModal.fromTemplateUrl('templates/modal/filter1.html', {
       scope: $scope,
       animation: 'slide-in-up'
@@ -8,7 +16,34 @@ connector.controller('DiscoverCtrl', function($scope,Chats, $ionicModal) {
     $scope.openModal = function () {
       $scope.modal.show();
     }
-  
+  $scope.select = function(interest){
+    // console.log("addinterest",interest)
+    
+    
+    var interestEdit = _.find($scope.interest, function (o){
+      console.log("interestarray",o)
+      if (interest == o.name) {
+        return o;
+        console.log("interestarray",o)
+      }
+      
+    });
+    if (interestEdit === undefined) {
+    $scope.interest.push({name:interest})
+    $scope.addInterest.interest = $scope.interest
+    $scope.colorchange = interestEdit
+    console.log("checknow", $scope.colorchange)
+    }else{
+     _.pull($scope.interest,interestEdit)
+     $scope.addInterest.interest = $scope.interest
+     $scope.colorchange = interestEdit
+     console.log("checknow", $scope.colorchange)
+    }
+    Chats.apiCallWithData("User/addInterests", $scope.addInterest, function (data) {
+      console.log("data is*****************", data)
+     
+    })
+  }
     $scope.closeModal = function () {
       $scope.modal.hide();
     };
