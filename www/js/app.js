53eb8259e42457e739e107f0e60f4ca0de8daf3e
sohan1 <shn619@gmail.com>
2017-12-25 15:50:35 +0530

    toaster

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var connector=angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
+var connector=angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova','ionic-toast'])
 
 .run(function ($ionicPlatform) {
   $ionicPlatform.ready(function () {
