490a2d4c0fd74f5af3e0a2bc52b2fe290152eec5
sohan1 <shn619@gmail.com>
2017-12-26 13:08:12 +0530

    locationctrl

--- www/js/controllers/LocationCtrl.js
+++ www/js/controllers/LocationCtrl.js
@@ -164,52 +164,6 @@ connector.controller('LocationCtrl', function ($scope, Chats, $state, $statePara
     }
   ];
 
-  // $scope.addCountry = function (data) {
-  //   console.log("**** inside addCountry ****", data);
-  //   if (data) {
-  //     $scope.dataToSave.country = data.countryName,
-  //       Chats.apiCallWithData("User/save", $scope.dataToSave, function (data) {
-  //         if (data.value == true) {
-
-  //         } else {
-
-  //         }
-  //       })
-  //   } else {
-  //     console.log("inside else part")
-  //   }
-
-  // if (data.state && data.country) {
-  // $scope.dataToSave.country = data.country,
-  //   $scope.dataToSave.state = data.state,
-  //   Chats.apiCallWithData("User/save", $scope.dataToSave, function (data) {
-  //     if (data.value == true) {
-
-  //     } else {
-
-  //     }
-  //   })
-  // } else {
-  //   console.log("inside else part")
-  // }
-
-  // }
-
-  // $scope.addState = function (data) {
-  //   if (data.state && data.country) {
-  //     $scope.dataToSave.country = data.country,
-  //       $scope.dataToSave.state = data.state,
-  //       Chats.apiCallWithData("User/save", $scope.dataToSave, function (data) {
-  //         if (data.value == true) {
-
-  //         } else {
-
-  //         }
-  //       })
-  //   } else {
-  //     console.log("inside else part")
-  //   }
-  // }
   $scope.goTOProfile = function (data) {
 
     if (data.selectedCountry && data.selectedState) {
@@ -246,61 +200,4 @@ connector.controller('LocationCtrl', function ($scope, Chats, $state, $statePara
     }
 
   }
-  // $scope.hideToast = function(){
-  //   ionicToast.hide();
-  // };
-  //  if($stateParams.userEmail){
-  // $scope.emailData={
-  //     email:$stateParams.userEmail
-  //   }
-  //   console.log("inside login stateparams", $scope.emailData)
-  //   Chats.apiCallWithData("User/getUser", $scope.emailData, function (data) {
-  //           $scope.formData = data.data;
-  //            $.jStorage.set("user",  $scope.formData);
-  //         $scope.dataToSave = {
-  //   _id: $.jStorage.get("user")._id
-  // }
-
-  //       })
-  //  }
-  //        $scope.dataToSave = {
-  //       _id: $.jStorage.get("user")._id
-  //     }
-  //      console.log(" $scope.dataToSave ", $scope.dataToSave )
-  //   $scope.addCountry = function (data) {
-  //   if (data.state && data.country) {
-  //     $scope.dataToSave.country = data.country,
-  //       $scope.dataToSave.state = data.state,
-  //       console.log("data to store", $scope.dataToSave)
-  //       Chats.apiCallWithData("User/save", $scope.dataToSave, function (data) {
-  //         if (data.value == true) {
-
-  //         } else {
-
-  //         }
-  //       })
-  //   } else {
-  //     console.log("inside else part")
-  //   }
-
-  // }
-  // $scope.addState = function (data) {
-  //   if (data.state && data.country) {
-  //     $scope.dataToSave.country = data.country,
-  //       $scope.dataToSave.state = data.state,
-  //       Chats.apiCallWithData("User/save", $scope.dataToSave, function (data) {
-  //         if (data.value == true) {
-
-  //         } else {
-
-  //         }
-  //       })
-  //   } else {
-  //     console.log("inside else part")
-  //   }
-  // }
-
-
-
-
 })
