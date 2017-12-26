5ad68504d5cde1b5455025dced4fb40bb6ccc239
sailirawnang <saili.rawnang@wohlig.com>
2017-12-26 11:36:41 +0530

    otp verify

--- www/js/controllers/OtpCtrl.js
+++ www/js/controllers/OtpCtrl.js
@@ -1,7 +1,7 @@
-connector.controller('OtpCtrl', function ($scope, $stateParams, $state, Chats,ionicToast) {
+connector.controller('OtpCtrl', function ($scope, $stateParams, $state, Chats, ionicToast) {
 
 
-    $scope.resendOtp = function (info) {
+    $scope.resendOtp = function () {
         $scope.data = {}
         $scope.data.email = $.jStorage.get("user").email
         Chats.apiCallWithData("User/sendOtp", $scope.data, function (info) {
@@ -10,6 +10,27 @@ connector.controller('OtpCtrl', function ($scope, $stateParams, $state, Chats,io
 
             }
         })
+
+    }
+    $scope.otp = function (info) {
+        if (info.digit1 && info.digit1 && info.digit3 && info.digit4) {
+            console.log("inside if")
+            $scope.data={}
+             $scope.data.otp=info.digit1+info.digit2+info.digit3+info.digit4
+             console.log(" $scope.data", $scope.data)
+                Chats.apiCallWithData("User/verifyOTPForResetPass",$scope.data, function (data) {
+                console.log("data is after verifyOTPForResetPass called", data);
+                if (data.value == true) {
+                  $state.go("home")
+
+                } else {
+                    console.log("Incorrect OTP!");
+                }
+            });
+        } else {
+            console.log("enter all the field")
+
+        }
 
     }
 })
