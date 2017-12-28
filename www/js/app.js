48f2b787a7f51846800ae0575c8a5af9d2e7dc66
sailirawnang <saili.rawnang@wohlig.com>
2017-12-28 17:13:18 +0530

    kwack integration

--- www/js/app.js
+++ www/js/app.js
@@ -93,7 +93,7 @@ var connector=angular.module('starter', ['ionic', 'starter.controllers', 'starte
     })
 
     .state('tab.trailer', {
-      url: '/trailer',
+      url: '/trailer/:newsid',
       views: {
         'tab-trailer': {
 
@@ -131,11 +131,13 @@ var connector=angular.module('starter', ['ionic', 'starter.controllers', 'starte
 
   .state('tab.discoverNews', {
     url: '/discoverNews',
+     cache:false,
     views: {
       'tab-discoverNews': {
 
         templateUrl: 'templates/tab-discoverNews.html',
-        controller: 'DiscoverNewsCtrl'
+        controller: 'DiscoverNewsCtrl',
+       
       }
     }
 
@@ -248,7 +250,7 @@ var connector=angular.module('starter', ['ionic', 'starter.controllers', 'starte
       controller: 'PollingInsideCtrl'
     })
     .state('debate', {
-      url: '/debate',
+      url: '/debate/:kwackId/:newsId',
       templateUrl: 'templates/debate.html',
       controller: 'DebateCtrl'
     })
