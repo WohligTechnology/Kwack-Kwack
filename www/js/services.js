6782d8cdfe300c4046a653a0f6b920a6d7754a2d
Pranay Joshi <pranayj1449517@gmail.com>
2018-01-02 19:35:06 +0530

    services

--- www/js/services.js
+++ www/js/services.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-var adminurl = "http://192.168.1.104:80/api/";
-=======
 var adminurl = "http://localhost:80/api/";
->>>>>>> d13a866e8565492991e12aafe1a03adb044b282f
 // var adminurl = "http://wohlig.io/api/";
 var imgurl = adminurl + "upload/";
 var imgpath = imgurl + "readFile?file=";
