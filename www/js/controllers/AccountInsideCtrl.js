fb7a6823603e37e5465dc6055cd3f0df9834cad4
sailirawnang <saili.rawnang@wohlig.com>
2018-01-30 11:59:57 +0530

    Login part is done properly

--- www/js/controllers/AccountInsideCtrl.js
+++ www/js/controllers/AccountInsideCtrl.js
@@ -1,2 +1,15 @@
-connector.controller('AccountInsieCtrl', function($scope) {
+connector.controller('AccountInsieCtrl', function ($scope,Chats, $state) {
+    $scope.deleteThisUser = function () {
+        $scope.datatoDelete = {}
+        $scope.datatoDelete.userId = $.jStorage.get('user');
+        
+        console.log("*********************I", $scope.datatoDelete)
+        Chats.apiCallWithData("User/setDeactiveUser",$scope.datatoDelete, function (data) {
+            if(data.value==true){
+                $state.go("signUp")
+            }
+            console.log("$$$$$$$$$$$$$$$$$$$$$$$",data)
+        })
+
+    };
 })
