ddcea4963a491895c9b08839da7f33d3c5eda2a2
sailirawnang <saili.rawnang@wohlig.com>
2018-01-31 11:34:11 +0530

    sign up page integration

--- www/js/controllers/MobileCtrl.js
+++ www/js/controllers/MobileCtrl.js
@@ -1,3 +1,18 @@
-connector.controller('MobileCtrl', function($scope) {
+connector.controller('MobileCtrl', function ($scope,Chats,ionicToast, $state) {
+    $scope.verifyMobile = function (data) {
+        console.log("********************", data)
+           $scope.data = {}
+        $scope.data.mobile = data.mobile
+        Chats.apiCallWithData("User/sendOtp", $scope.data, function (info) {
+            console.log("otp",info)
+            if (info.value == true) {
+                ionicToast.show('Otp Sent Successfully', 'top', false, 2500);
+                 $state.go("otp")
 
+            }else{
+                ionicToast.show('Enter valid number', 'top', false, 2500);
+            }
+        })
+
+    }
 })
