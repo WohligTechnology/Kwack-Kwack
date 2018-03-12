15b4e8c4f88b7eda80dcf6c027c9d24d70fad7fd
sailirawnang <saili.rawnang@wohlig.com>
2018-03-12 11:33:52 +0530

    sms integration

--- www/js/controllers/ForgotPassCtrl.js
+++ www/js/controllers/ForgotPassCtrl.js
@@ -4,12 +4,13 @@ connector.controller('ForgotPassCtrl', function ($scope, Chats, ionicToast, $sta
             mobile: forgot.mobile
         }
         $.jStorage.set("mobile", $scope.mobile);
-        Chats.apiCallWithData("User/sendOtp", forgot, function (otp) {
-            console.log("otp", otp)
-            if (otp.value == true) {
+        Chats.apiCallWithData("User/sendOtp", forgot, function (info) {
+            if (info.data == "sms-sent") {
                 ionicToast.show('Otp Sent Successfully', 'top', false, 2500);
                 $state.go("otp")
-            } else {
+            } else if (info.data == "INV-NUMBER") {
+                ionicToast.show('Not valid Number', 'top', false, 2500);
+            } else if (data.value == false) {
                 ionicToast.show('Please enter correct Mobile Number', 'top', false, 2500);
             }
         })
--- www/js/controllers/Success.js
+++ www/js/controllers/Success.js
@@ -1,13 +1,23 @@
 connector.controller('SuccessCtrl', function ($scope, $state,Chats) {
     $scope.success = $.jStorage.get("mobile")
-    $scope.dataToSend = {}
-    $scope.dataToSend.mobile = $.jStorage.get("user").mobile
+    // $scope.dataToSend = {}
+    // $scope.dataToSend.mobile = $.jStorage.get("user").mobile,
+        $scope.smsMessage = "Thank You for Signing Up!"
+                $scope.smsObj = {
+                    "message": "kwack",
+                    "sender": "Kwackk",
+                    "sms": [{
+                        "to": $.jStorage.get("user").mobile,
+                        "message":  $scope.smsMessage,
+                        "sender": "Kwackk"
+                    }]
+                };
     $scope.nextstate = function () {
         if ($scope.success != null) {
             $state.go("confirmpass")
         } else {
             $state.go("location")
-            Chats.apiCallWithData("User/sendWelcomeMsg", $scope.dataToSend, function (data) {
+            Chats.apiCallWithData("Config/sendSMS",  $scope.smsObj, function (data) {
                 if (data.value == true) {}
             })
         }
