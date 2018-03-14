17830d4e53504a233f2d36aeae6e9aad74baffc9
sailirawnang <saili.rawnang@wohlig.com>
2018-03-14 15:53:36 +0530

    done jstorage set in location ctrl

--- www/js/controllers/LoginCtrl.js
+++ www/js/controllers/LoginCtrl.js
@@ -1,8 +1,11 @@
 connector.controller('LoginCtrl', function ($scope, $cordovaFileTransfer, Chats, $state, $stateParams, $cordovaOauthUtility, $cordovaOauth, ionicToast, $http) {
   if ($.jStorage.get("user")) {
     if (_.isEmpty($.jStorage.get("user").state)) {
+      console.log("************************inside if condition")
       $state.go("mobile")
     } else {
+      console.log("************************inside else condition")
+      
       $state.go("tab.explore")
     }
   }
